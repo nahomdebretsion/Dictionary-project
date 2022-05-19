2a71b7b32128cd5bbcddef3a4c72366dc0292001
nahomdebretsion <nahomdebretsion@gmail.com>
2022-05-19 14:31:30 -0500

    Initial commit

---
+++ Project-dictionary/dict.js
@@ -0,0 +1,61 @@
+
+// request based on jquery
+// $(document).ready(()=>{
+//   $('#lookup').click(()=>{
+//     var word= $('#word').val();
+//     $.ajax({
+//       "url": "http://localhost:2021/", 
+//       "data": {"word": word},  // user input to be send to the server, ?word=''
+//       "type": "GET",
+//       "success": (data)=>{
+//         window.updateHtml(data);
+//       }, 
+//       //"error": ()=> console.log('Error ocured from the server')
+//       });
+//   })
+
+// }
+// );
+$(document).ready(()=>{
+  $('#lookup').click(()=>{
+    var word= $('#word').val();
+    //$.ajax({
+  // config for your database
+  var config = {
+  user: 'sa',
+  password: 'mypassword',
+  server: 'localhost',
+  database: 'SchoolDB',
+  options: { trustServerCertificate: true }
+  };
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
+    // create Request object
+    var request = new sql.Request();
+    request.query('select * from Student', function (err, recordset) {
+      if (err) console.log(err)
+      // send records as a response
+      res.send(recordset);
+      });
+      });
+      });
+    });
+$(document).ready(()=>{
+  $("#word").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#lookup").click();
+    }
+  });
+})
+
+// function showHTML(data){
+//   // console.log(data);
+//   window.updateHtml(data);
+
+// }
+
+// function errorHandler(){
+//   console.log('Erroe ocured from the server');
+// }
+
+
