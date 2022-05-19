a0cee38825764a7f8bdec2dbd3a2c5c105b73ba6
nahomdebretsion <nahomdebretsion@gmail.com>
2022-05-19 14:42:13 -0500

    nnn

--- Project-dictionary/dict.js
+++ Project-dictionary/dict.js
@@ -1,45 +1,22 @@
 
-// request based on jquery
-// $(document).ready(()=>{
-//   $('#lookup').click(()=>{
-//     var word= $('#word').val();
-//     $.ajax({
-//       "url": "http://localhost:2021/", 
-//       "data": {"word": word},  // user input to be send to the server, ?word=''
-//       "type": "GET",
-//       "success": (data)=>{
-//         window.updateHtml(data);
-//       }, 
-//       //"error": ()=> console.log('Error ocured from the server')
-//       });
-//   })
-
-// }
-// );
+//request based on jquery
 $(document).ready(()=>{
   $('#lookup').click(()=>{
     var word= $('#word').val();
-    //$.ajax({
-  // config for your database
-  var config = {
-  user: 'sa',
-  password: 'mypassword',
-  server: 'localhost',
-  database: 'SchoolDB',
-  options: { trustServerCertificate: true }
-  };
-  sql.connect(config, function (err) {
-    if (err) console.log(err);
-    // create Request object
-    var request = new sql.Request();
-    request.query('select * from Student', function (err, recordset) {
-      if (err) console.log(err)
-      // send records as a response
-      res.send(recordset);
+    $.ajax({
+      "url": "http://localhost:2021/", 
+      "data": {"word": word},  // user input to be send to the server, ?word=''
+      "type": "GET",
+      "success": (data)=>{
+        window.updateHtml(data);
+      }, 
+      "error": ()=> console.log('Error ocured from the server')
       });
-      });
-      });
-    });
+  })
+
+}
+);
+
 $(document).ready(()=>{
   $("#word").keyup(function(event) {
     if (event.keyCode === 13) {
@@ -48,6 +25,32 @@ $(document).ready(()=>{
   });
 })
 
+// }
+// );
+// $(document).ready(()=>{
+//   $('#lookup').click(()=>{
+//     var word= $('#word').val();
+//     //$.ajax({
+//   // config for your database
+//   var config = {
+//   user: 'sa',
+//   password: 'mypassword',
+//   server: 'localhost',
+//   database: 'SchoolDB',
+//   options: { trustServerCertificate: true }
+//   };
+//   sql.connect(config, function (err) {
+//     if (err) console.log(err);
+//     // create Request object
+//     var request = new sql.Request();
+//     request.query('select * from Student', function (err, recordset) {
+//       if (err) console.log(err)
+//       // send records as a response
+//       res.send(recordset);
+//       });
+//       });
+//       });
+//     });
 // function showHTML(data){
 //   // console.log(data);
 //   window.updateHtml(data);
